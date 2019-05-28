515eaf714825e2462fe3b401288be552b25d7e8e
Sean Shookman <sms112788@gmail.com>
2019-05-28 11:20:06 -0500

    #9 | Updates test for non-skele projects

--- test/test_systems_parsing_skeleParser.py
+++ test/test_systems_parsing_skeleParser.py
@@ -14,6 +14,15 @@ class TestParser(TestCase):
         argKeys = list(vars(args).keys())
         self.assertEqual(argKeys, ["job", "env", "skip_build", "native"])
 
+    def test_parseArgs_non_skelebot(self):
+        config = sb.objects.config.Config()
+        sbParser = sb.systems.parsing.skeleParser.SkeleParser(config, "test")
+        args = sbParser.parseArgs()
+
+        argKeys = list(vars(args).keys())
+        self.assertEqual(argKeys, ["job"])
+
+
     def test_description(self):
         config = sb.objects.config.Config(name="test-project", description="A test project", version="0.1.0", skelebotVersion="0.2.0")
         sbParser = sb.systems.parsing.skeleParser.SkeleParser(config, "test")
@@ -26,7 +35,7 @@ A test project
 Version: 0.1.0
 Environment: test
 Skelebot Version (project): 0.2.0
-Skelebot Version (installed): 0.2.0
+Skelebot Version (installed): 0.2.1
 -----------------------------------"""
 
         self.assertEqual(description, expectedDescription)
