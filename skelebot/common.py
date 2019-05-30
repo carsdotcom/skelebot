5a1e184c3e0d57bae52d59679d21b97ff708add3
Sean Shookman <sms112788@gmail.com>
2019-05-30 16:18:02 -0500

    #17 | Fixing bug with job param/argument choices

--- skelebot/common.py
+++ skelebot/common.py
@@ -16,6 +16,8 @@ PLUGINS_HOME = "{home}/plugins".format(home=SKELEBOT_HOME)
 
 EXTENSION_COMMAND = {"py":"python ", "R":"Rscript ", "sh":"./"}
 IMAGE_VERSION = {
+    "0.2.4": 0.1,
+    "0.2.3": 0.1,
     "0.2.2": 0.1,
     "0.2.1": 0.1,
     "0.2.0": 0.1,
--- test/test_systems_parsing_skeleParser.py
+++ test/test_systems_parsing_skeleParser.py
@@ -7,21 +7,30 @@ import os
 class TestParser(TestCase):
 
     def test_parseArgs(self):
-        config = sb.objects.config.Config(name="test-project", description="A test project", version="0.1.0", skelebotVersion="0.2.0")
+        arg1 = sb.objects.param.Param(name="start")
+        arg2 = sb.objects.param.Param(name="end", choices=["now", "never"])
+        param1 = sb.objects.param.Param(name="days", alt="d", default=10)
+        param2 = sb.objects.param.Param(name="env", alt="e", default="local", choices=["local", "prod"])
+        job = sb.objects.job.Job(name="test", mode="d", source="test.py", help="TEST", args=[arg1, arg2], params=[param1, param2])
+        config = sb.objects.config.Config(name="test-project", description="A test project", version="0.1.0", skelebotVersion="0.2.0", jobs=[job])
         sbParser = sb.systems.parsing.skeleParser.SkeleParser(config, "test")
-        args = sbParser.parseArgs()
+        args = sbParser.parseArgs(["test", "2019", "never", "-d", "20", "--env", "prod"])
 
         argKeys = list(vars(args).keys())
-        self.assertEqual(argKeys, ["job", "env", "skip_build", "native"])
+        self.assertEqual(args.job, "test")
+        self.assertEqual(args.start, "2019")
+        self.assertEqual(args.end, "never")
+        self.assertEqual(args.days, "20")
+        self.assertEqual(args.env, "prod")
 
-    def test_parseArgs_non_skelebot(self):
+    def test_parseArgs_non_skelebot_scaffold(self):
         config = sb.objects.config.Config()
         sbParser = sb.systems.parsing.skeleParser.SkeleParser(config, "test")
-        args = sbParser.parseArgs()
+        args = sbParser.parseArgs(["scaffold"])
 
         argKeys = list(vars(args).keys())
-        self.assertEqual(argKeys, ["job"])
-
+        self.assertEqual(args.job, "scaffold")
+        self.assertEqual(args.existing, False)
 
     def test_description(self):
         config = sb.objects.config.Config(name="test-project", description="A test project", version="0.1.0", skelebotVersion="0.2.0")
@@ -35,7 +44,7 @@ A test project
 Version: 0.1.0
 Environment: test
 Skelebot Version (project): 0.2.0
-Skelebot Version (installed): 0.2.3
+Skelebot Version (installed): 0.2.4
 -----------------------------------"""
 
         self.assertEqual(description, expectedDescription)
