801c1e0f0ab61f5398e32e15d477a7f40dad2f19
Sean Shookman <sms112788@gmail.com>
2019-06-05 16:57:29 -0500

    Allows python scripts to display output via Docker (#11)

--- skelebot/common.py
+++ skelebot/common.py
@@ -8,23 +8,12 @@ DESCRIPTION = """
 -----------------------------------
 Version: {pVersion}
 Environment: {env}
-Skelebot Version (project): {sbVersion}
-Skelebot Version (installed): {version}
+Skelebot Version: {version}
 -----------------------------------"""
 SKELEBOT_HOME = "~/.skelebot"
 PLUGINS_HOME = "{home}/plugins".format(home=SKELEBOT_HOME)
 
-EXTENSION_COMMAND = {"py":"python ", "R":"Rscript ", "sh":"./"}
-IMAGE_VERSION = {
-    "0.2.4": 0.1,
-    "0.2.3": 0.1,
-    "0.2.2": 0.1,
-    "0.2.1": 0.1,
-    "0.2.0": 0.1,
-    "0.1.2": 0.1,
-    "0.1.1": 0.1,
-    "0.1.0": 0.1
-}
+IMAGE_VERSION = 0.1
 LANGUAGE_IMAGE = {
     "Python": {
         "base": "skelebot/python-base:{version}",
@@ -41,4 +30,4 @@ LANGUAGE_DEPENDENCIES = {
     "R":["data.table", "here", "stringr", "readr", "testthat", "yaml"]
 }
 
-EXT_COMMAND= {"py":"python ", "R":"Rscript ", "sh":"./"}
+EXT_COMMAND= {"py":"python -u ", "R":"Rscript ", "sh":"./"}
