eddb585097afda9eed8f88a6d4a4552418b3fd91
Sean Shookman <sms112788@gmail.com>
2019-08-06 13:39:41 -0500

    More lint fixes

--- test/plugins/addNumbers/addNumbers.py
+++ test/plugins/addNumbers/addNumbers.py
@@ -9,7 +9,7 @@ class AddNumbers(Component):
        subparsers.add_parser("addNumbers", help="adds the numbers 1 and 2 to get 3!")
        return subparsers
 
-    def execute(self):
+    def execute(self, config, args):
         a = 1
         b = 2
         print(a, " + ", b, " = ", a + b)
