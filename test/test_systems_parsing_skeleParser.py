1be497c3cae7bfbb47f7163140f1829012761b51
Sean Shookman <sms112788@gmail.com>
2019-11-06 11:13:30 -0600

    version bump and changelog update

--- test/test_systems_parsing_skeleParser.py
+++ test/test_systems_parsing_skeleParser.py
@@ -49,7 +49,7 @@ A test project
 -----------------------------------
 Version: 0.1.0
 Environment: test
-Skelebot Version: 1.7.5
+Skelebot Version: 1.7.6
 -----------------------------------"""
 
         self.assertEqual(description, expectedDescription)
