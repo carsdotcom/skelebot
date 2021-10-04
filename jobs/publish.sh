f2a47e8de6fcdfdd786f9b0726465b578e16d44e
Joao Moreira <13685125+jagmoreira@users.noreply.github.com>
2021-10-04 09:52:36 -0400

    Add support for python 3.9 projects.

--- jobs/publish.sh
+++ jobs/publish.sh
@@ -21,7 +21,9 @@ docker push skelebot/r-redshift:latest
 docker build -t skelebot/python-base:3.6 -t skelebot/python-base:latest base-images/python-base/3.6/
 docker build -t skelebot/python-base:3.7 base-images/python-base/3.7/
 docker build -t skelebot/python-base:3.8 base-images/python-base/3.8/
-docker push skelebot/python-base # Newer Docker clinets will push latest by default and will need to do `docker push skelebot/python-base --all-tags`
+docker build -t skelebot/python-base:3.9 base-images/python-base/3.9/
+# Newer Docker clients will push latest by default and will need to do `docker push skelebot/python-base --all-tags`
+docker push skelebot/python-base
 
 # Build and Publish python-krb
 docker build -t skelebot/python-krb base-images/python-krb/
